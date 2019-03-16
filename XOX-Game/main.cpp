0d352b5dec28e6be00cd7e64f7a5e86311179318
cagritrk <haliscagri@gmail.com>
2019-03-16 12:41:53 +0300

    Initial Commit

---
+++ XOX-Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XOX-Game
+//
+//  Created by Çağrı Türk on 16.03.2019.
+//  Copyright © 2019 Çağrı Türk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
